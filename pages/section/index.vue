a3936bf5c7c8665232c03391500b04a861ade5c3
Maxime <mdeecode@gmail.com>
2021-05-11 11:31:16 +0200

    now date

---
+++ pages/section/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">section A</h1>
+      <NuxtLink to="/">Home</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
